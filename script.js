ecbb3dc137e42b40ab9fa6aca1f07210b9cce6e1
Dal ferro Christian <christiandalferro@gmail.com>
2025-06-27 11:58:28 +0200

    modif url2

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-
+/*
 fetch("https://whatyearisit-backend-nu-seven.vercel.app/year")
     .then(res => res.json())
     .then(d=>{
         console.log(d);
         document.querySelector('#year').textContent = d.year;
     })
+*/
+let date = Date()
+document.querySelector('#year').textContent = date
